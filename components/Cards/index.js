09f4c1969eebe2e778655d0fba6da6bbeeab7843
Uzias Rivera <uziasrivera@gmail.com>
2019-09-06 09:10:44 -0700

    adding content to index.js Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,27 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response=>{
+    const objKeys = (Object.keys(response.data.articles))
+    console.log(response.data.articles[objKeys[0]])
+})
+
+function cardCreator(obj){
+    const card = document.createElement('div')
+    const cardHeadline = document.createElement('div')
+    const author = document.createElement('div')
+    const imgWrap = document.createElement('div')
+    const image = document.createElement('img')
+    const authorName = document.createElement('span')
+
+    //adding classes
+    card.classList.add('card')
+    cardHeadline.classList.add('headline')
+    author.classList.add('author')
+    imgWrap.classList.add('img-container')
+
+    //adding content
+
+}
